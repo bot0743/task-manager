f534bf6cdc75dac726ddbda59832187d0ffbacf2
bot0743 <156990592+bot0743@users.noreply.github.com>
2026-02-04 11:07:16 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Конфигурация Firebase
 const firebaseConfig = {
-    apiKey: "ВАШ_API_KEY",
-    authDomain: "ВАШ_PROJECT.firebaseapp.com",
-    projectId: "ВАШ_PROJECT_ID",
-    storageBucket: "ВАШ_PROJECT.appspot.com",
-    messagingSenderId: "ВАШ_SENDER_ID",
-    appId: "ВАШ_APP_ID"
+  apiKey: "AIzaSyAVlZwbARYWzPWmSjnc24t0FOSlKP4UmOg",
+  authDomain: "task-manager-c0fa9.firebaseapp.com",
+  projectId: "task-manager-c0fa9",
+  storageBucket: "task-manager-c0fa9.firebasestorage.app",
+  messagingSenderId: "288647961766",
+  appId: "1:288647961766:web:357c40995536c6bbbb6867"
 };
 
 // Инициализация Firebase
